b2ad19a1181d0feb3f761a1980271ef55124dc48
meghamachhi <machhimegha2@gmail.com>
2025-07-24 16:46:44 +0530

    fix: use env var for API base URL

--- api.ts
+++ api.ts
@@ -1,6 +1,6 @@
 import { ITask } from "./types/tasks";
 
-// const baseUrl = 'http://localhost:3001';
+const baseUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001';
 
 export const getAllTodos = async (): Promise<ITask[]> => {
     const res = await fetch(`${baseUrl}/tasks`);
