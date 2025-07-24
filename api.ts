66dbd13bf14772b0af3e5852a44e51770063a645
meghamachhi <machhimegha2@gmail.com>
2025-07-24 16:32:59 +0530

    fix: Disable local API for Vercel deployment

--- api.ts
+++ api.ts
@@ -1,6 +1,6 @@
 import { ITask } from "./types/tasks";
 
-const baseUrl = 'http://localhost:3001';
+// const baseUrl = 'http://localhost:3001';
 
 export const getAllTodos = async (): Promise<ITask[]> => {
     const res = await fetch(`${baseUrl}/tasks`);
